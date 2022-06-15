9a95219466cb49a7bdcfa311ef2d9b3a0331b232
Suman Chapai <sumanchapai@gmail.com>
2022-06-15 18:37:31 -0500

    zoom and panes (#11)

--- client/components/LC/lc.js
+++ client/components/LC/lc.js
@@ -1,92 +1,61 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
 import { useState, useEffect } from "react";
-import { myDateFormatString, minDateForChartZoom, millisecondsInAYear } from "utils";
-import { withData } from "hoc";
-import {useStarData} from "hooks"
-import {useRouter} from "next/router"
 import {
-  Chart as ChartJS,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Tooltip,
-  Legend,
-} from 'chart.js';
-import {Scatter} from 'react-chartjs-2'
-import {fluxToMagnitude, isDateBetween} from 'utils'
+    myDateFormatString,
+    minDateForChartZoom,
+    millisecondsInAYear,
+} from "utils";
+import { withData } from "hoc";
+import { useStarData } from "hooks";
+import { useRouter } from "next/router";
+import {
+    Chart as ChartJS,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Tooltip,
+    Legend,
+} from "chart.js";
+import { Scatter } from "react-chartjs-2";
+import { fluxToMagnitude, isDateBetween } from "utils";
 ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend);
-
 
 export const BuildLC = ({ number, data }) => {
     useEffect(() => {
-        async function registerZoom(){
-            const zoomPlugin = (await import('chartjs-plugin-zoom')).default
-            ChartJS.register(zoomPlugin)
+        async function registerZoom() {
+            const zoomPlugin = (await import("chartjs-plugin-zoom")).default;
+            ChartJS.register(zoomPlugin);
         }
-        registerZoom()
-    }, [])
-    const [from, setFrom] = useState(
-        myDateFormatString(new Date("2003-01-02"))
-    );
-    const [to, setTo] = useState(myDateFormatString(new Date()));
+        registerZoom();
+    }, []);
     const [starData, setStarData] = useState(data);
+    const router = useRouter();
     useEffect(() => {
-        setStarData(data.filter(dataPoint => isDateBetween(dataPoint.date, from, to)))
-    }, [from, to])
-    function handleFrom(e) {
-        setFrom(e.target.value);
-    }
-    function handleTo(e) {
-        setTo(e.target.value);
-    }
-    const router = useRouter()
-    useEffect(() => {
-        setStarData(data)
-    }, [router.query])
-    return (
-        <div>
-            <div className="flex flex-wrap gap-x-16 my-8">
-                <TextField
-                    id="dateFrom"
-                    label="From"
-                    type="date"
-                    value={from}
-                    onChange={handleFrom}
-                    sx={{ width: 220 }}
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                />
-                <TextField
-                    id="dateTo"
-                    label="To"
-                    type="date"
-                    value={to}
-                    onChange={handleTo}
-                    sx={{ width: 220 }}
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                />
-            </div>
-            <Curve data={starData} starNumber={number}/>
-        </div>
-    );
+        setStarData(data);
+    }, [router.query]);
+    return <Curve data={starData} starNumber={number} />;
 };
 
 function Curve({ data: rawData, starNumber }) {
-    const rawDataWithZerosMasked = rawData.filter(dataPoint => dataPoint.flux !== 0)
-    const dates = rawData.map(x => new Date(x.date).getTime())
-    const xMin = minDateForChartZoom()
-    const xMax = Math.max(...dates) + millisecondsInAYear() 
+    const rawDataWithZerosMasked = rawData.filter(
+        (dataPoint) => dataPoint.flux !== 0
+    );
+    const dates = rawData.map((x) => new Date(x.date).getTime());
+    const magnitudes = rawDataWithZerosMasked.map((x) =>
+        fluxToMagnitude(x.flux)
+    );
+    const yMin = Math.min(...magnitudes);
+    const yMax = Math.max(...magnitudes);
+    const xMin = minDateForChartZoom();
+    const xMax = Math.max(...dates) + millisecondsInAYear();
     const options = {
         scales: {
             x: {
                 beginsAtZero: false,
                 ticks: {
-                    callback: function(value, index, ticks){
-                        return new Date(value).toLocaleDateString()
+                    callback: function (value, index, ticks) {
+                        return new Date(value).toLocaleDateString();
                     },
                 },
             },
@@ -97,61 +66,68 @@ function Curve({ data: rawData, starNumber }) {
                     text: "Magnitude",
                     color: "#222",
                     display: true,
-                }
-            }
+                },
+            },
         },
         plugins: {
             tooltip: {
                 callbacks: {
-                    label: function(context){
-                        let label = context.dataset.label || '';
-                        if (label){
-                            label = new Date(context.parsed.x).toLocaleDateString() + "  ";
+                    label: function (context) {
+                        let label = context.dataset.label || "";
+                        if (label) {
+                            label =
+                                new Date(
+                                    context.parsed.x
+                                ).toLocaleDateString() + "  ";
                         }
-                        if (context.parsed.y !== null){
+                        if (context.parsed.y !== null) {
                             label += context.parsed.y.toFixed(6);
                         }
                         return label;
-                    }
-                }
+                    },
+                },
             },
             zoom: {
                 pan: {
-                    enabled: true
+                    enabled: true,
+                    mode: "xy",
                 },
-              limits: {
-                    x: {min: xMin, max: xMax}
+                limits: {
+                    x: { min: xMin, max: xMax },
+                    y: { min: yMin - 0.1, max: yMax + 0.1 },
                 },
                 zoom: {
-              wheel: {
-                enabled: true,
-              },
-              pinch: {
-                enabled: true
-              },
-              mode: 'x',
-        }
+                    wheel: {
+                        enabled: true,
+                    },
+                    pinch: {
+                        enabled: false,
+                    },
+                    mode: "x",
+                },
             },
         },
         elements: {
             point: {
-                borderColor: 'black',
-                backgroundColor: '#ddd',
-            }
+                borderColor: "black",
+                backgroundColor: "#ddd",
+            },
         },
-    }
+    };
     const data = {
         datasets: [
             {
                 label: `#${starNumber}`,
-                data:  rawDataWithZerosMasked.map(point => ({x: new Date(point.date).getTime(), y: fluxToMagnitude(point.flux)}))
-            }
-        ]
-    }
-
+                data: rawDataWithZerosMasked.map((point) => ({
+                    x: new Date(point.date).getTime(),
+                    y: fluxToMagnitude(point.flux),
+                })),
+            },
+        ],
+    };
 
     return (
-        <div>
+        <div className="mt-8">
             <Scatter options={options} data={data} />
         </div>
     );
