16e17b8fbac1a8e8e0cebc3d7f319804e0639717
suman chapai <sumanchapai@gmail.com>
2022-07-20 11:03:05 -0500

    edit start database script

--- backend/scripts/startDatabase.sh
+++ backend/scripts/startDatabase.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+docker restart m23db ||
 docker run --name m23db -p 5433:5432\
     -e POSTGRES_PASSWORD=mysecretpassword \
-    -d postgres \
+    -d postgres 
+postgrest ~/Desktop/light-curves/backend/m23db.conf
