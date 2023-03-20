2b646c2a684810ceb6ee4c3cc81d90b61e644da0
suman chapai <sumanchapai@gmail.com>
2023-03-20 15:44:14 -0500

    Enhance frontend components

--- client/components/LC/selectStar.js
+++ client/components/LC/selectStar.js
@@ -6,56 +6,56 @@ import { withData } from "hoc";
 import { useStars } from "hooks";
 
 export function SelectStar({ starsToFilter, minimal, defaultValue = "" }) {
-    const [allStars, allStarsError] = useStars();
-    const [myStar, setMyStar] = React.useState(defaultValue);
-    const router = useRouter();
-    useEffect(() => {
-        if (myStar) router.push(`/lc/${myStar}`);
-    }, [myStar]);
-    return (
-        <div>
-            {!minimal && <h2 className="text-sm mb-6">Choose/Type:</h2>}
-            <div className="">
-                {React.cloneElement(
-                    withData(StarSelectField, allStars, allStarsError),
-                    {
-                        value: myStar,
-                        setMyStar: setMyStar,
-                        starsToFilter: starsToFilter,
-                        defaultValue: defaultValue,
-                    }
-                )}
-            </div>
-        </div>
-    );
+  const [allStars, allStarsError] = useStars();
+  const [myStar, setMyStar] = React.useState(defaultValue);
+  const router = useRouter();
+  useEffect(() => {
+    if (myStar) router.push(`/lc/${myStar}`);
+  }, [myStar, router]);
+  return (
+    <div>
+      {!minimal && <h2 className="text-sm mb-6">Choose/Type:</h2>}
+      <div className="">
+        {React.cloneElement(
+          withData(StarSelectField, allStars, allStarsError),
+          {
+            value: myStar,
+            setMyStar: setMyStar,
+            starsToFilter: starsToFilter,
+            defaultValue: defaultValue,
+          }
+        )}
+      </div>
+    </div>
+  );
 }
 
 function StarSelectField({ data: options, value, setMyStar, starsToFilter }) {
-    const [inputValue, setInputValue] = useState("");
-    const router = useRouter();
+  const [inputValue, setInputValue] = useState("");
+  const router = useRouter();
 
-    useEffect(() => {
-        const newStar = parseInt(router.query.slug);
-        if (newStar) setMyStar(newStar.toString());
-    }, [router.query]);
+  useEffect(() => {
+    const newStar = parseInt(router.query.slug);
+    if (newStar) setMyStar(newStar.toString());
+  }, [router.query, setMyStar]);
 
-    return (
-        <div className="">
-            <Autocomplete
-                value={value}
-                onChange={(event, newValue) => {
-                    setMyStar(newValue);
-                }}
-                inputValue={inputValue}
-                onInputChange={(event, newInputValue) => {
-                    setInputValue(newInputValue);
-                }}
-                id="star-select"
-                options={options.filter((x) => x != starsToFilter)}
-                sx={{ width: 300 }}
-                renderInput={(params) => <TextField {...params} label="Star" />}
-                isOptionEqualToValue={(option, value) => option.id === value.id}
-            />
-        </div>
-    );
+  return (
+    <div className="">
+      <Autocomplete
+        value={value}
+        onChange={(event, newValue) => {
+          setMyStar(newValue);
+        }}
+        inputValue={inputValue}
+        onInputChange={(event, newInputValue) => {
+          setInputValue(newInputValue);
+        }}
+        id="star-select"
+        options={options.filter((x) => x != starsToFilter)}
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="Star" />}
+        isOptionEqualToValue={(option, value) => option.id === value.id}
+      />
+    </div>
+  );
 }
--- client/hooks/index.js
+++ client/hooks/index.js
@@ -2,12 +2,12 @@ export * from "./data.js";
 import useSWR from "swr";
 
 const buildLabelsUrl = (starNumber) =>
-    `https://api.github.com/repos/LutherAstrophysics/stars/issues/${starNumber}/labels`;
+  `https://api.github.com/repos/LutherAstrophysics/stars/issues/${starNumber}/labels`;
 
 const githubLabelsFetcher = (starNumber) =>
-    fetch(buildLabelsUrl(starNumber)).then((r) => r.json());
+  fetch(buildLabelsUrl(starNumber)).then((r) => r.json());
 
 export function useLabels(number) {
-    const { data, error } = useSWR(number, githubLabelsFetcher);
-    return [data, error];
+  const { data, error } = useSWR(number, githubLabelsFetcher);
+  return [data, error];
 }
