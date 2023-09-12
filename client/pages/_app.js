cb9f02cbc8dbeeba2e52ef03a59bb5f2fac464bb
suman chapai <sumanchapai@gmail.com>
2023-09-12 07:11:07 -0500

    Make secondary the default data set

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -3,7 +3,7 @@ import { MyContext } from "../contexts/myContext";
 import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  const [contextValue, setContextValue] = useState({ primaryData: true, step2012Applied: true });
+  const [contextValue, setContextValue] = useState({ primaryData: false, step2012Applied: true });
   return (
     <MyContext.Provider
       value={{ value: contextValue, setValue: setContextValue }}
